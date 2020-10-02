6b811cbda91d53f6a877d23b617e24b595b4b5cd
YONI SETIAWAN <yonisetiawan1990@gmail.com>
2020-10-02 10:42:29 +0700

    Add files via upload

---
+++ precache-manifest.5c135c40aa3f9e2d6e090814170d4767.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44efaf65aafd65f81dfe9f4ae799f9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01c3e1406c8717ad46c2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1c445307d756370a1458",
+    "url": "/static/js/2.757af166.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.757af166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01c3e1406c8717ad46c2",
+    "url": "/static/js/main.95c3e07b.chunk.js"
+  },
+  {
+    "revision": "9f10ad7796608ffb84b7",
+    "url": "/static/js/runtime-main.1b50fb31.js"
+  }
+]);
